a979b727c5266c5c01acfb876bcfb951db6d8f23
JDagsa <jayden.dagsa@gmail.com>
2022-11-27 17:12:25 +0800

    Fixed errors (<a> in layout.js)

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,5 @@
-import autoprefixer from 'autoprefixer';
 import Image from 'next/image'
+import Link from 'next/link'
 
 const customLoader = ({ src }) => {
     return `/${src}`
@@ -10,20 +10,22 @@ export default function Layout({ children }) {
         <div>
             <header className="flex flex-col dark:bg-black">
                 <div className="max-w-screen-md mx-10 my-4">
-                <a href='https://cnc.novushierosolymis.ml'>
-                    <Image
-                    loader={customLoader}
-                    alt=''
-                    src="CNC.svg"
-                    width={143}
-                    height={54}
-                    style={{
-                        width: 'auto',
-                        margin: '0',
-                        padding: '0'
-                        }}
-                    />
-                </a>
+                <Link href='https://cnc.novushierosolymis.ml'>
+                    <a href='https://cnc.novushierosolymis.ml'>
+                        <Image
+                        loader={customLoader}
+                        alt=''
+                        src="CNC.svg"
+                        width={143}
+                        height={54}
+                        style={{
+                            width: 'auto',
+                            margin: '0',
+                            padding: '0'
+                            }}
+                        />
+                    </a>
+                </Link>
                 </div>
                 <div className="bg-gradient-to-tr from-cncRed to-cncGold px-10 py-1">
                     <h1 className="text-white text-4xl font-semibold">
