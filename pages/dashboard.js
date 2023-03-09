d184899d338c8170c39a7904c7178f2bd1cf10b7
JDagsa <jayden.dagsa@gmail.com>
2023-03-09 17:21:03 +0800

    Fix problems with apps initialize

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,7 @@
 import { auth } from "@/lib/firebase";
 import Link from "next/link";
-import CNCNewsLogo from '../public/images/CNC.svg'
+import CNCLogo from '../public/images/CNC.svg'
+import CNCNewsLogo from '../public/images/CNC_News.svg'
 import { BsBoxArrowRight } from "react-icons/bs";
 import { useUser } from "@/context/user";
 
@@ -14,7 +15,7 @@ export default function Dashboard() {
         return (
             <main className="bg-slate-50 dark:bg-slate-900 dark:text-slate-50 text-slate-900 w-screen h-screen">
                 <nav className="flex justify-between px-6 py-4 bg-gray-100 dark:bg-gray-800">
-                    <p className="flex items-center gap-1 text-2xl"><CNCNewsLogo className="w-24 h-11" alt="Logo of CNC"/> Staff Dashboard</p>
+                    <p className="flex items-center gap-1 text-2xl"><CNCLogo className="w-24 h-11" alt="Logo of CNC"/> Staff Dashboard</p>
                     <button onClick={e => signOut()} title="Log out" className="text-3xl"><BsBoxArrowRight /></button>
                 </nav>
                 <section className="px-12 py-8">
